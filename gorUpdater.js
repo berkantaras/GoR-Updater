a3ea197e868aeb798296c15e7d252dfba82e62a3
berkantaras <40351932+berkantaras@users.noreply.github.com>
2020-05-19 16:40:45 +0300

    Update gorUpdater.js

--- gorUpdater.js
+++ gorUpdater.js
@@ -1,9 +1,9 @@
 function gorUpdater() {
   
-  var ss = SpreadsheetApp.openById("1zLHxxP20goVDXN39FHGihQDdXcsbZbToVJLDJwwqMvU"); //Enter the spreadsheet id number here
+  var ss = SpreadsheetApp.openById("1jYv5VzmQIHE572kRBmLouLXMCBddzGyy4ydWGehzLWQ"); //Handicap Group A
   var sheets = ss.getSheets();
   
-  for(i = 2; i <= 53; i++) { //53 for the Go League, 45 for the Handicap Go League
+  for(i = 2; i <= 45; i++) {
     
     var str = "D" + String(i);
     
@@ -16,7 +16,73 @@ function gorUpdater() {
     var json = response.getContentText();
     var list = JSON.parse(json);
     
-    var str2 = "O" + String(i);
+    str2 = "O" + String(i);
+  
+    sheets[2].getRange(str2).setValue(list.Gor);
+    
+  }
+  
+  var ss = SpreadsheetApp.openById("1neFq-utdwVBPwfE9BKVxiAiORw8ablCKBQb3nuVfOtM"); //Handicap Group B
+  var sheets = ss.getSheets();
+  
+  for(i = 2; i <= 45; i++) {
+    
+    var str = "D" + String(i);
+    
+    var pin = sheets[2].getRange(str).getValue();
+  
+    var url = "https://www.europeangodatabase.eu/EGD/GetPlayerDataByPIN.php?pin=" + pin;
+  
+    var response = UrlFetchApp.fetch(url, {'muteHttpExceptions': true});
+  
+    var json = response.getContentText();
+    var list = JSON.parse(json);
+    
+    str2 = "O" + String(i);
+  
+    sheets[2].getRange(str2).setValue(list.Gor);
+    
+  }
+  
+  var ss = SpreadsheetApp.openById("1J9s0tq8MKPOckoaJCU9oUgu6TyHuI_a-ydgtQEEt_QQ"); //Group A
+  var sheets = ss.getSheets();
+  
+  for(i = 2; i <= 53; i++) {
+    
+    var str = "D" + String(i);
+    
+    var pin = sheets[2].getRange(str).getValue();
+  
+    var url = "https://www.europeangodatabase.eu/EGD/GetPlayerDataByPIN.php?pin=" + pin;
+  
+    var response = UrlFetchApp.fetch(url, {'muteHttpExceptions': true});
+  
+    var json = response.getContentText();
+    var list = JSON.parse(json);
+    
+    str2 = "O" + String(i);
+  
+    sheets[2].getRange(str2).setValue(list.Gor);
+    
+  }
+  
+  var ss = SpreadsheetApp.openById("1zLHxxP20goVDXN39FHGihQDdXcsbZbToVJLDJwwqMvU"); //Group B
+  var sheets = ss.getSheets();
+  
+  for(i = 2; i <= 53; i++) {
+    
+    var str = "D" + String(i);
+    
+    var pin = sheets[2].getRange(str).getValue();
+  
+    var url = "https://www.europeangodatabase.eu/EGD/GetPlayerDataByPIN.php?pin=" + pin;
+  
+    var response = UrlFetchApp.fetch(url, {'muteHttpExceptions': true});
+  
+    var json = response.getContentText();
+    var list = JSON.parse(json);
+    
+    str2 = "O" + String(i);
   
     sheets[2].getRange(str2).setValue(list.Gor);
     
